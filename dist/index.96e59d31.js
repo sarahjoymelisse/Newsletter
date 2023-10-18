45affaafc6b07531d839834888e8ba3862b837f3
Melisse <58097465+sarahjoymelisse@users.noreply.github.com>
2023-10-18 14:11:35 +0800

    Update to Parcel

---
+++ dist/index.96e59d31.js
@@ -0,0 +1,3 @@
+const e=document.querySelector("form"),t=document.querySelector(".card"),r=document.querySelector(".card-2");e.addEventListener("submit",e=>{e.preventDefault(),t.classList.add("hide"),r.classList.remove("hide")});//# sourceMappingURL=index.96e59d31.js.map
+
+//# sourceMappingURL=index.96e59d31.js.map
